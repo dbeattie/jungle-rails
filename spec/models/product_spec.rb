3999aad542d4fddc74741227897e35747aa5141b
dbeattie <darren@fettle.ca>
2020-02-10 20:30:32 +0000

    add product model spec

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    before(:each) do
+      @category = Category.new(id: 5, name: "Sports Equipment")
+      @product = Product.new(name: "Basketball", price_cents: 2300, quantity: 12, category: @category)
+    end
+
+    it "is a valid product with all fields filled" do
+      expect(@product).to be_valid
+    end
+
+    it "is not a product with no name" do
+      @product.name = nil
+      expect(@product).to_not be_valid
+    end  
+
+    it "is not a product with no price" do
+      @product.price_cents = nil
+      expect(@product).to_not be_valid
+    end   
+
+    it "is not a product with no quantity" do
+      @product.quantity = nil
+      expect(@product).to_not be_valid
+    end  
+
+    it "is not a product with no category" do
+      @product.category = nil
+      expect(@product).to_not be_valid      
+    end
+  end
+end
+
+#validates :name, presense: true
+#validates :price, presense: true
+#validates :quantity, presense: true
+#validates :category, presense: true
